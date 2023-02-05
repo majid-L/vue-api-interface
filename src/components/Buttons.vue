6e51e8ab0afbf4c2d006971a40bbe499b1bf2df5
3782291211 <Laklouk98@hotmail.com>
2023-02-05 20:23:19 +0000

    Adding reusable button component

---
+++ src/components/Buttons.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="btn btn-primary" @click="onClick()">Patterns</button>
+</template>
+
+<script>
+export default {
+    name: 'Buttons',
+    methods: {
+        onClick() {
+            fetch('https://automatrixapi.pythonanywhere.com/api/users')
+            .then(res => res.json())
+            .then(({users}) => console.log(users))
+        }
+    }
+}
+</script>
+
+<style scope>
+button {
+    font-size: 20px;
+}
+</style>
