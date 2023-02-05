ba62b970b59ee231a9089e226ad6620ab0951527
3782291211 <Laklouk98@hotmail.com>
2023-02-05 22:05:37 +0000

    Updating CSS class names

--- src/components/Pattern.vue
+++ src/components/Pattern.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="card">
         <div class="card-body">
-          <h5 class="card-title">"{{ pattern.pattern_name }}""</h5>
+          <h5 class="card-title">"{{ pattern.pattern_name }}"</h5>
           <h6 class="card-subtitle mb-2 text-muted">by {{ pattern.username }}</h6>
           <p class="card-text"><span>Pattern id:</span> {{ pattern._id }}</p>
-          <p class="card-text pattern-body"><span>Pattern body:</span> {{ pattern.pattern_body }}</p>
+          <p class="card-text --hide-overflow"><span>Pattern body:</span> {{ pattern.pattern_body }}</p>
           <p class="card-text"><span>Created at:</span> {{ pattern.created_at }}</p>
         </div>
     </div>
