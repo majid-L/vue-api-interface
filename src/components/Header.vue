10820d6e0bab1046da25a015fdf87ceba868394d
3782291211 <Laklouk98@hotmail.com>
2023-02-05 20:23:33 +0000

    Adding header component with button group

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Buttons/>
+    </header>
+</template>
+
+<script>
+import Buttons from './Buttons.vue';
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'Hello'
+        }
+    },
+    components: {
+        Buttons
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    font-family: 'Luckiest Guy', cursive;
+    font-size: 50px;
+    margin: 60px auto 40px
+}
+
+header {
+    margin-bottom: 50px
+}
+</style>
