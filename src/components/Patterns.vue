07ce73b42a237266664cf8506343ac0efa6ddbbf
3782291211 <Laklouk98@hotmail.com>
2023-02-05 20:24:26 +0000

    Changing patterns style to grid display

---
+++ src/components/Patterns.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="patterns_flexbox">
+<div :key="pattern._id" v-for="pattern in patterns">
+<Pattern :pattern="pattern" />
+</div>
+</div>
+</template>
+
+<script>
+import Pattern from './Pattern.vue';
+
+export default {
+    name: 'Patterns',
+    props: {
+        patterns: Array
+    },
+    components: {
+        Pattern
+    }
+}
+</script>
+
+<style scoped>
+.patterns_flexbox {
+  width: 90%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, max-content));
+  justify-content: center;
+  gap: 15px;
+  margin: 0 auto
+}
+</style>
