5660e1c1dfab84f6650040a32e4aad02e85f918c
3782291211 <Laklouk98@hotmail.com>
2023-02-05 22:03:12 +0000

    Implementing routing with dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,50 @@
-<template>
-<Header title="API interface"/>
+<!-- <template>
+<Header title="API interface" @fetch-patterns="fetchPatterns" @fetch-users="fetchUsers"/>
 <Patterns :patterns="patterns"/>
+<Users :users="users"/>
+</template> -->
+
+<template>
+  <NavigationBar />
+  <Header title="API interface" />
+  <component :is="currentView" />
 </template>
 
 <script>
+import NavigationBar from './components/NavigationBar.vue';
+import Home from './components/Home.vue';
 import Header from './components/Header.vue';
-import Patterns from './components/Patterns.vue'
+import Patterns from './components/Patterns.vue';
+import Users from './components/Users.vue';
+import NotFound from './components/NotFound.vue';
+
+const routes = {
+  '/': Home,
+  '/patterns': Patterns,
+  '/users': Users
+};
 
 export default {
   name: 'App',
   components: {
     Header,
-    Patterns
+    NavigationBar
   },
   compatConfig: { MODE: 3 },
   data() {
     return {
-      patterns: [],
-      users: []
+      currentPath: window.location.hash
     }
   },
-  methods: {
-    async fetchPatterns() {
-      const response = await fetch('https://automatrixapi.pythonanywhere.com/api/patterns');
-      const {patterns} = await response.json();
-      return patterns;
-    } 
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
   },
-  async created() {
-    this.patterns = await this.fetchPatterns();
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash;
+    })
   }
 }
 </script>
@@ -70,7 +86,7 @@ li {
     margin: 2px 0
 }
 
-.pattern-body {
+.--hide-overflow {
     display: -webkit-box;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
@@ -79,5 +95,14 @@ li {
 
 span {
     font-weight: bold;
+}
+
+.flexbox {
+  width: 90%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, max-content));
+  justify-content: center;
+  gap: 15px;
+  margin: 0 auto
 }
 </style>
