0952f2023f77e5a77c8626e66748cdb29a963376
3782291211 <Laklouk98@hotmail.com>
2023-02-06 03:34:44 +0000

    Adding fetch function to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,35 @@
 <template>
-    <h1>Welcome!</h1>
+    <h2 @click="onClick()">Welcome!</h2>
     <p>This application provides a simple front end interface for one of my back end API projects.</p>
+    <div v-for="endpoint in endpoints">
+        <div>hello</div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            endpoints: []
+        }
+    },
+    methods: {
+        onClick() {
+            console.log(123)
+        },
+        async fetchEndpoints() {
+            const response = await fetch('https://automatrixapi.pythonanywhere.com');
+            const {endpoints} = await response.json();
+            return endpoints;
+        },
+        async created() {
+            this.endpoints = endpoints;
+        }
+    }
+}
+
+</script>
 
 <style scoped>
 p {
