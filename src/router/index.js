de41c7651a52a038e75b414043f18e9cf4c0cc38
3782291211 <Laklouk98@hotmail.com>
2023-02-06 14:20:17 +0000

    Adding new paths and routes to vue router

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,18 @@ import Patterns from '../views/Patterns';
 import Users from '../views/Users';
 import NotFound from '../views/NotFound';
 import NewPattern from '../views/NewPattern';
+import NewUser from '../views/NewUser';
+import DeleteConfirmation from '../views/DeleteConfirmation';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/patterns', name: 'patterns', component: Patterns},
     {path: '/users', name: 'users', component: Users},
+    {path: '/users/:_id', name: 'single user', component: NewUser, props: true},
     {path: '/users/:username/patterns', name: 'userPatterns', component: PatternsByUsername, props: true},
     {path: '/patterns/new', name: 'new pattern', component: NewPattern},
+    {path: '/users/new', name: 'new user', component: NewUser},
+    {path: '/deleted', name: 'deleted', component: DeleteConfirmation},
     {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ];
 
