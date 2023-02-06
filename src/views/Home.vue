2251d1ba0d48b8e168fea245bee2ecaad83f5da8
3782291211 <Laklouk98@hotmail.com>
2023-02-06 04:59:45 +0000

    Adding endpoints list to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-    <h2 @click="onClick()">Welcome!</h2>
+    <h2>Welcome!</h2>
     <p>This application provides a simple front end interface for one of my back end API projects.</p>
-    <div v-for="endpoint in endpoints">
-        <div>hello</div>
+
+<section>
+<div class="list-group" v-for="endpoint in endpoints">
+  <div class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">{{ endpoint.name }}</h5>
     </div>
+    <p class="mb-1">{{endpoint.description}}</p>
+    <small v-if="endpoint.example_request">Example request: {{ endpoint.example_request }}</small>
+</div>
+</div>
+</section>
+
 </template>
 
 <script>
@@ -15,17 +25,14 @@ export default {
         }
     },
     methods: {
-        onClick() {
-            console.log(123)
-        },
         async fetchEndpoints() {
             const response = await fetch('https://automatrixapi.pythonanywhere.com');
             const {endpoints} = await response.json();
             return endpoints;
-        },
-        async created() {
-            this.endpoints = endpoints;
         }
+    },
+    async created() {
+        this.endpoints = await this.fetchEndpoints();
     }
 }
 
@@ -33,6 +40,21 @@ export default {
 
 <style scoped>
 p {
-    font-size: 20px
+    font-size: 16px
+}
+
+.list-group {
+    width: 90%;
+    max-width: 1000px;
+    margin: 0 auto;
+}
+
+.list-group-item {
+    margin: 3px;
+    text-align: left;
+}
+
+h5 {
+    font-weight:600;
 }
 </style>
