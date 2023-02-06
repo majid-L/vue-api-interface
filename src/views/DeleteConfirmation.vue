7386768ef9398d9df76724e642b35850374f5271
3782291211 <Laklouk98@hotmail.com>
2023-02-06 14:19:55 +0000

    Adding delete confirmation page.

---
+++ src/views/DeleteConfirmation.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>Successfully deleted {{ this.$route.query.type }}.</h3>
+   <button @click="() => $router.go(-1)" type="button" class="btn btn-warning">Go back</button>
+</template>
+
+<script>
+export default {
+    name: "DeleteConfirmation"
+}
+</script>
+
+<style scoped>
+
+button {
+    margin: 20px
+}
+
+</style>
