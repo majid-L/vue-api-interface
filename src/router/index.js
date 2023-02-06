deb803dae513908353d88156ac66814b015a187a
3782291211 <Laklouk98@hotmail.com>
2023-02-06 05:00:36 +0000

    Adding new paths to router package

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import Home from '../views/Home';
 import Patterns from '../views/Patterns';
 import Users from '../views/Users';
 import NotFound from '../views/NotFound';
+import NewPattern from '../views/NewPattern';
 
 const routes = [
-    {path: '/', name: 'home', component: Home},
+    {path: '/', name: 'Home', component: Home},
     {path: '/patterns', name: 'patterns', component: Patterns},
     {path: '/users', name: 'users', component: Users},
     {path: '/users/:username/patterns', name: 'userPatterns', component: PatternsByUsername, props: true},
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    {path: '/patterns/new', name: 'new pattern', component: NewPattern},
+    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ];
 
 const router = createRouter({
