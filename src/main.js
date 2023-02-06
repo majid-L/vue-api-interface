c7d8ce5fa8f65529d623db3dd551eafa0dfad3c0
3782291211 <Laklouk98@hotmail.com>
2023-02-06 19:32:15 +0000

    Adding user component to global scope.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import router from "./router/index";
 import Pattern from './components/Pattern';
+import User from './components/User';
 
 const app = createApp(App);
 app.component('Pattern', Pattern);
+app.component('User', User);
 app.use(router).mount("#app");
