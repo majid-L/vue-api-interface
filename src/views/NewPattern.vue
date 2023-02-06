5ee751c5fe5870e143103e5fb98f680680ccb086
3782291211 <Laklouk98@hotmail.com>
2023-02-06 10:45:15 +0000

    Adding error handling, alerts and loading state to component.

--- src/views/NewPattern.vue
+++ src/views/NewPattern.vue
@@ -4,23 +4,34 @@
 <form @submit.prevent="onSubmit">
   <div class="mb-3">
     <label for="username" class="form-label">Username</label>
-    <input v-model="username" type="text" class="form-control" id="username">
+    <input v-model.lazy="username" type="text" class="form-control" id="username" placeholder="Enter username">
   </div>
   <div class="mb-3">
     <label for="pattern_name" class="form-label">Pattern name</label>
-    <input v-model="pattern_name" type="text" class="form-control" id="pattern_name">
+    <input v-model.lazy="pattern_name" type="text" class="form-control" id="pattern_name" placeholder="Enter pattern name">
   </div>
   <div class="mb-3">
     <label for="pattern_body" class="form-label">Pattern Body</label>
-    <input v-model="pattern_body" type="text" class="form-control" id="pattern_body">
-    <div class="form-text">This needs to be in a specific format. Please visit the patterns page to take a look.</div>
+    <textarea v-model.lazy="pattern_body" type="text" class="form-control" id="pattern_body" placeholder="e.g. 1011111111 0000010011 1111100000 1110100000 1011110111 1010001111 1111111111 0011100000 1111111100 0001010000"/>
+    <div class="form-text">This needs to be in a specific format. Visit the patterns page to take a look.</div>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
+<div v-if="error" class="alert alert-danger" role="alert">
+    {{ error }}
+</div>
+
+<div v-if="success" class="alert alert-success" role="alert">
+  <p>Your new pattern has been successfully added.</p>
+  <Pattern :pattern="newPattern"/>
+</div>
+
 </template>
 
 <script>
+import Pattern from '../components/Pattern.vue';
+
 export default {
     name: 'NewPattern',
     data() {
@@ -28,23 +39,44 @@ export default {
             newPattern: [],
             username: '',
             pattern_name: '',
-            pattern_body: ''
+            pattern_body: '',
+            error: false,
+            success: false,
+            loading: false
         }
     },
     methods: {
+        async onSubmit() {
+            this.success = false;
+            if (!this.username || !this.pattern_name || !this.pattern_body) {
+                this.error = "Please ensure that you complete all fields.";
+            } else if (!/^(\d+\s)+\d+$/.test(this.pattern_body)) {
+                this.error = "Incorrect pattern body format. Please try again.";
+            } else {
+                this.newPattern = await this.postNewPattern();
+            }
+        },
+        
         async postNewPattern() {
+            this.success = false;
+            this.error = false;
             const response = await fetch('https://automatrixapi.pythonanywhere.com/api/patterns', {
                 method: 'POST',
-                body: {username, pattern_name, pattern_body}
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username: this.username, 
+                    pattern_name: this.pattern_name, 
+                    pattern_body: this.pattern_body})
             });
-            const {pattern} = await response.json();
-            return pattern;
-        }
-    },
-    onSubmit(e) {
-        console.log('hi')
-        if (!username || !pattern_name || !patern_body) {
-            console.log('saydgsafdugyisa')
+            if (response.ok) {
+              const {pattern} = await response.json();
+              this.success = true;
+              return pattern;
+            } else {
+              const {msg} = await response.json();
+              this.loading = false;
+              this.error = msg;
+            }
         }
     }
 }
@@ -54,7 +86,8 @@ export default {
 
 form {
     margin: 30px auto;
-    width: 70%;
+    width: 80%;
+    max-width: 900px;
     background-color: whitesmoke;
     padding: 20px;
     color: black;
@@ -65,5 +98,11 @@ form {
 label {
     margin: 0
 }
+
+/*
+use this tyle if changing from textarea to input for pattern body
+input:placeholder-shown {
+  text-overflow: ellipsis;
+}*/
 
 </style>
