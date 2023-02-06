20462a3c11bdf6244687589976255bdcf39dcc15
3782291211 <Laklouk98@hotmail.com>
2023-02-06 19:35:32 +0000

    Adding styles

--- src/components/Pattern.vue
+++ src/components/Pattern.vue
@@ -4,11 +4,11 @@
         <div class="button-group">
           <form @submit.prevent="onSubmit" class="d-flex">
           <input class="form-control me-2" type="text" v-model="newPatternName" placeholder="New pattern name" aria-label="Update">
-          <button :disabled="!newPatternName" class="btn btn-outline-success" type="submit">Update</button>
+          <button :disabled="!newPatternName" class="btn btn-outline-success" type="submit"><span v-if="loadingUpdate" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>{{ !loadingUpdate ? 'Update' : null}}</button>
         </form>
         <button @click="deletePattern" class="btn btn-danger" type="button">
-            <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-            {{ !loading ? 'Delete' : 'Deleting...'}}
+            <span v-if="loadingDelete" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            {{ !loadingDelete ? 'Delete' : 'Deleting...'}}
         </button>
         </div>
           <h5 class="card-title">"{{ pattern.pattern_name }}"</h5>
@@ -16,6 +16,8 @@
           <p class="card-text"><span>Pattern id:</span> {{ pattern._id }}</p>
           <p class="card-text --hide-overflow"><span>Pattern body:</span> {{ pattern.pattern_body }}</p>
           <p class="card-text"><span>Created at:</span> {{ pattern.created_at }}</p>
+
+          <p class="error-msg" v-if="error">{{ error }}</p>
         </div>
     </div>
 </template>
@@ -29,22 +31,40 @@ export default {
     data() {
         return {
             newPatternName: '',
-            loading: false
+            loadingDelete: false,
+            loadingUpdate: false,
+            error: false
         }
     },
     methods: {
         async deletePattern() {
-            this.loading = true;
+            this.error = false;
+            this.loadingDelete = true;
             const response = await fetch(`https://automatrixapi.pythonanywhere.com/api/patterns/${this.pattern._id}`, { method: 'DELETE'});
-            this.loading = false;
+            this.loadingDelete = false;
             if (response.ok) {
                 this.$router.push({path: '/deleted', query: {type: 'pattern'}})
             } else {
-                return
+                const {msg} = await response.json();
+                this.error = msg;
             }
         },
-        onSubmit() {
-            
+        async onSubmit() {
+            this.error = false;
+            this.loadingUpdate = true;
+            const response = await fetch(`https://automatrixapi.pythonanywhere.com/api/patterns/${this.pattern._id}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({"pattern_name": this.newPatternName})
+            });
+            this.loadingUpdate = false;
+            if (response.ok) {
+                const {updated_pattern} = await response.json();
+                this.pattern.pattern_name = updated_pattern.pattern_name;
+            } else {
+                const {msg} = await response.json();
+                this.error = msg;
+            }
         }
     }
 }
@@ -59,11 +79,20 @@ input:placeholder-shown {
   text-overflow: ellipsis;
 }
 
+button {
+    color: whitesmoke
+}
+
 .spinner-border {
   width: 15px !important;
   height: 15px !important;
   font-size: 10px;
   margin: 0 5px 0 0;
+}
+
+.error-msg {
+    color: red;
+    margin: 10px auto 0
 }
 
 </style>
