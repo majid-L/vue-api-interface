2ec27ab021cb004b6622b777a77576912de5ee81
3782291211 <Laklouk98@hotmail.com>
2023-02-06 01:39:45 +0000

    Declaring global component in main.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import router from "./router/index";
+import Pattern from './components/Pattern';
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.component('Pattern', Pattern);
+app.use(router).mount("#app");
