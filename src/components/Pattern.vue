053643d4ad3d5e0b228141af95ed4cf8df87bc1b
3782291211 <Laklouk98@hotmail.com>
2023-02-06 14:17:10 +0000

    Adding delete button and functionality to pattern card component

--- src/components/Pattern.vue
+++ src/components/Pattern.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="card">
         <div class="card-body">
+        <div class="button-group">
+          <form @submit.prevent="onSubmit" class="d-flex">
+          <input class="form-control me-2" type="text" v-model="newPatternName" placeholder="New pattern name" aria-label="Update">
+          <button :disabled="!newPatternName" class="btn btn-outline-success" type="submit">Update</button>
+        </form>
+        <button @click="deletePattern" class="btn btn-danger" type="button">
+            <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            {{ !loading ? 'Delete' : 'Deleting...'}}
+        </button>
+        </div>
           <h5 class="card-title">"{{ pattern.pattern_name }}"</h5>
           <h6 class="card-subtitle mb-2 text-muted">by {{ pattern.username }}</h6>
           <p class="card-text"><span>Pattern id:</span> {{ pattern._id }}</p>
@@ -15,6 +25,27 @@ export default {
     name: 'Pattern',
     props: {
         pattern: Object
+    },
+    data() {
+        return {
+            newPatternName: '',
+            loading: false
+        }
+    },
+    methods: {
+        async deletePattern() {
+            this.loading = true;
+            const response = await fetch(`https://automatrixapi.pythonanywhere.com/api/patterns/${this.pattern._id}`, { method: 'DELETE'});
+            this.loading = false;
+            if (response.ok) {
+                this.$router.push({path: '/deleted', query: {type: 'pattern'}})
+            } else {
+                return
+            }
+        },
+        onSubmit() {
+            
+        }
     }
 }
 </script>
@@ -22,6 +53,17 @@ export default {
 <style scoped>
 * {
     color: black
+}
+
+input:placeholder-shown {
+  text-overflow: ellipsis;
+}
+
+.spinner-border {
+  width: 15px !important;
+  height: 15px !important;
+  font-size: 10px;
+  margin: 0 5px 0 0;
 }
 
 </style>
